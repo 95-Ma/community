8782013ef911afd75bd4d8d024a6933d339528db
iris_glo <1318707669_glo@qq.com>
2021-01-01 21:35:37 +0800

    引入bootstrap依赖，在index.html文件引入bootstrap，复制修改bootstrap官方导航栏代码，删除HelloController.java文件，添加IndexController.java文件，控制所有请求转发到index.html页面

--- src/main/java/motor/community/controller/IndexController.java
+++ src/main/java/motor/community/controller/IndexController.java
@@ -10,11 +10,10 @@ import org.springframework.web.bind.annotation.RequestParam;
  * @create 2021-01-01-19:59
  */
 @Controller
-public class HelloController {
+public class IndexController {
 
-    @GetMapping("/hello")
-    public String hello(@RequestParam(name="name")String name, Model model){
-        model.addAttribute("name",name);
-        return "hello";
+    @GetMapping("/")
+    public String index() {
+        return "index";
     }
 }
